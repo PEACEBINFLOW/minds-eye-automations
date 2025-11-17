1b6f1507ec71aed544686de2036a4d64cb3f1622
Peace Thabiwa <peacethabibinflow@proton.me>
2025-11-17 11:19:05 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+console.log("Mind's Eye Automations booted.");
+
+export * from "./actions/refreshCognitivePool";
+export * from "./actions/runSearchSnapshot";
+export * from "./actions/longTermMemoryWriter";
+
+export * from "./triggers/gmailTrigger";
+export * from "./triggers/driveTrigger";
+export * from "./triggers/calendarTrigger";
+export * from "./triggers/meetTrigger";
